40c4caec252226129eeefb5e12bbbe5d3e66a952
Saggiee <sagarkhule.2003@gmail.com>
2024-10-18 09:25:53 +0530

    add nginx and script

--- cart/urls.py
+++ cart/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.urls import path
 from . import views
@@ -9,3 +10,6 @@ urlpatterns = [
     path('cart/', views.view_cart, name='view_cart'),
     path('clear-cart/', views.clear_cart, name='clear_cart'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
--- payment/urls.py
+++ payment/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('checkout/', views.checkout, name='checkout'),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('order_history/', views.order_history, name='order_history'),
     path('recent_order/', views.recent_order, name='recent_order'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
